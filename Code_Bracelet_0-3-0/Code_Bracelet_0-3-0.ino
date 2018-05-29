1849db8fe06f1bae67f2e65ed5c80f14ea2644fb
Stuntflier <couzinet.guillaume@gmail.com>
2018-05-29 17:04:04 +0200

    ajout code recpteur

--- Code_Bracelet_0-3-0/Code_Bracelet_0-3-0.ino
+++ Code_Bracelet_0-3-0/Code_Bracelet_0-3-0.ino
@@ -2,11 +2,10 @@
 #include <LoRa.h>
 #include <SoftwareSerial.h>
 
-//const byte LEDPIN = 4;
+const byte LEDPIN = 4;
 const byte INTERRUPTPIN = 3;
 volatile byte STATE = LOW;
 const int taille_msg = 26;
-char MESSAGE[taille_msg] = "180197Guillaume";
 
 int counter = 0;
 const int temps_attente_BT = 30;
@@ -40,33 +39,17 @@ void setup() {
   //digitalWrite(LEDPIN, STATE);
 }
 
-void envoyer(char tab[]) {
+void envoyer() {
   Serial.print("Sending packet: ");
   Serial.println(counter);
 
   // send packet
   LoRa.beginPacket();
-  LoRa.print(tab);
+  LoRa.print("Alert receive");
   LoRa.print(counter);
   LoRa.endPacket();
 
   counter++;
-}
-
-bool testco2() {
-  int r = 0;
-  bool test = false;
-  
-  //Serial.println(t);
-  if(mySerial.available()){
-    r = mySerial.read();
-  }
-  
-  if (r != 0 ){
-    test = true;
-  }
-  
-  return test;
 }
 
 void blink() {
@@ -74,52 +57,15 @@ void blink() {
 }
 
 void alarme_BT() {
-  mySerial.println("Declencher l alarme");
-}
-
-void modification() {
-  int i = 0;
-  char newMessage[taille_msg];
-  while(i<taille_msg)
-  {
-    char c;
-    c = mySerial.read();
-    newMessage[i] = c;
-    i++;
-  }
-  *MESSAGE = newMessage;
-}
-
-void alerte() {
-  if(testco2())
-  {
-    alarme_BT();
-  }
-  else
-  {
-    envoyer(MESSAGE);
-  }
+  mySerial.println("Alerte");
 }
 
 void loop() {
+  digitalWrite(LEDPIN, STATE)
   if(STATE)
   {
-    alerte();
+    alarme_BT();
+    envoyer();
+    
   }
-  
-  if(mySerial.available()){
-    int i = 0;
-    char newMessage[taille_msg];
-    while(mySerial.available() && i < taille_msg)
-    {
-      char c;
-      c = mySerial.read();
-      newMessage[i] = c;
-      i++;
-    }
-    *MESSAGE = newMessage;
-
-    Serial.println(MESSAGE);
-  }
-
 }
